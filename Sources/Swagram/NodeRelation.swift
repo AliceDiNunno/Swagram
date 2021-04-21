30c9be0416a3b56247e6ec07ddfad2dcd496ee22
Alice Di Nunno <alice@dinunno.fr>
2021-04-21 23:47:57 +0200

    [style] improved NodeRelation complexity

--- Sources/Swagram/NodeRelation.swift
+++ Sources/Swagram/NodeRelation.swift
@@ -8,32 +8,27 @@
 import Foundation
 
 public class NodeRelation<NodeItem> {
-    private var _parent = NodeRelation<NodeItem>?(nil)
-    private var _children = [NodeRelation<NodeItem>]()
-    private var _siblings = [NodeRelation<NodeItem>]()
-    
-    public var parent: NodeRelation<NodeItem>? { _parent }
-    public var children: [NodeRelation<NodeItem>] { _children }
-    public var siblings: [NodeRelation<NodeItem>] { _siblings }
-    
+    private(set) var parent = NodeRelation<NodeItem>?(nil)
+    private(set) var children = [NodeRelation<NodeItem>]()
+    private(set) var siblings = [NodeRelation<NodeItem>]()
     
     var item: NodeItem
     
     var relationText = ""
     
     func setParent(relation: NodeRelation<NodeItem>) {
-        _parent = relation
-        relation._children.append(self)
+        parent = relation
+        relation.children.append(self)
     }
     
     func setSibling(relations: [NodeRelation<NodeItem>]) {
-        _siblings = relations
+        siblings = relations
     }
     
     func setChildren(relations: [NodeRelation<NodeItem>]) {
-        _children = relations
+        children = relations
         for relation in relations {
-            relation._parent = self
+            relation.parent = self
         }
         
     }
